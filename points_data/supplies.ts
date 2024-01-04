3b39996138c6a27b6af68a9b18f8e54c94f2137a
t0m0120 <highwest.t@gmail.com>
2024-01-04 10:11:52 +0900

    七尾 支援拠点 1/3→1/4へリンク修正

--- points_data/supplies.ts
+++ points_data/supplies.ts
@@ -11,13 +11,13 @@ type Tree = {
 };
 
 const supply: RawSentou[] = [
-  ['七尾市役所', 'https://www.city.nanao.lg.jp/bosai/mail/20240103_11.html', 37.043071556614414, 136.96758679523793],
-  ['七尾東部中学校', 'https://www.city.nanao.lg.jp/bosai/mail/20240103_11.html', 37.027878852072014, 136.969258293254],
-  ['七尾市立和倉小学校', 'https://www.city.nanao.lg.jp/bosai/mail/20240103_11.html', 37.08307169263842, 136.91804133761894],
-  ['七尾市北大呑地区コミュニティセンター', 'https://www.city.nanao.lg.jp/bosai/mail/20240103_11.html', 37.040807234143934, 137.04633274598115],
-  ['田鶴浜地区コミュニティセンター', 'https://www.city.nanao.lg.jp/bosai/mail/20240103_11.html',37.05979241606568, 136.8952321173631],
-  ['七尾市立中島小学校', 'https://www.city.nanao.lg.jp/bosai/mail/20240103_11.html',37.121722300000016, 136.84271456509103],
-  ['七尾市 能登島地区コミュニティセンター', 'https://www.city.nanao.lg.jp/bosai/mail/20240103_11.html',37.12619904157633, 136.99657256564498],
+  ['七尾市役所', 'https://www.city.nanao.lg.jp/bosai/mail/20240104_4.html', 37.043071556614414, 136.96758679523793],
+  ['七尾東部中学校', 'https://www.city.nanao.lg.jp/bosai/mail/20240104_4.html', 37.027878852072014, 136.969258293254],
+  ['七尾市立和倉小学校', 'https://www.city.nanao.lg.jp/bosai/mail/20240104_4.html', 37.08307169263842, 136.91804133761894],
+  ['七尾市北大呑地区コミュニティセンター', 'https://www.city.nanao.lg.jp/bosai/mail/20240104_4.html', 37.040807234143934, 137.04633274598115],
+  ['田鶴浜地区コミュニティセンター', 'https://www.city.nanao.lg.jp/bosai/mail/20240104_4.html',37.05979241606568, 136.8952321173631],
+  ['七尾市立中島小学校', 'https://www.city.nanao.lg.jp/bosai/mail/20240104_4.html',37.121722300000016, 136.84271456509103],
+  ['七尾市 能登島地区コミュニティセンター', 'https://www.city.nanao.lg.jp/bosai/mail/20240104_4.html',37.12619904157633, 136.99657256564498],
 ];
 
 const formatted: Tree[] = supply.map(([name, text, lat, lng]) => ({
